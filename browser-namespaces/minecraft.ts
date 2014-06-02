0264a13c156deab86876e5d586fb6d18041dc343
nitram509 <maki@bitkings.de>
2014-06-02 09:08:36 +0200

    initieal commit

---
+++ browser-namespaces/minecraft.ts
@@ -0,0 +1,19 @@
+module minecraft {
+
+    export module enemies {
+
+        export interface NamedEnemy {
+            name: string;
+        }
+
+        export class skeleton implements NamedEnemy {
+            name:string;
+        }
+
+        export class creeper implements NamedEnemy {
+            name:string;
+            power:number = 100;
+        }
+    }
+
+}
