69e6688b69d705d68672d246082c6ac37933e1e3
nitram509 <maki@bitkings.de>
2014-08-08 23:57:45 +0200

    implemented serving HAL browser rom ZIP file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 /// <reference path="typings/express/express.d.ts" />
+/// <reference path="typings/express-static-zip/express-static-zip.d.ts" />
 /// <reference path="typings/node/node.d" />
 
 import express = require('express');
-import GoldenTestData = require('GoldenTestData');
+import expressStaticZip = require('express-static-zip');
+import GoldenTestData = require('./GoldenTestData');
 
 var gtd = new GoldenTestData();
 
 var app = express();
-var port = 8080;
+var port = Number(process.env.PORT || 8080);
+
+// serving 3rd party code from ZIP file, just because statistics in Github pages ;-)
+app.use('/hal', expressStaticZip(__dirname + '/../static/hal.zip'));
+
 app.listen(port);
 console.log('Express started on port ' + port);
---
+++ server/src/typings/express-static-zip/express-static-zip.d.ts
@@ -0,0 +1,9 @@
+declare module "express-static-zip" {
+
+  // minimalistic declaration file, which is needed by Typescript compiler
+
+  function e(zipFileName: string, options?: any): any;
+
+  export = e;
+
+}
