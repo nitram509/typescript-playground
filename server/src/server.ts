5ddad090331ff1e9d961f6357d0f0518a001fd52
nitram509 <maki@bitkings.de>
2014-08-10 22:21:48 +0200

    implemented index.hal.json and a first simple /mobs resource

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 /// <reference path="typings/express/express.d.ts" />
-/// <reference path="typings/express-static-zip/express-static-zip.d.ts" />
 /// <reference path="typings/node/node.d" />
+/// <reference path="typings/express-static-zip/express-static-zip.d.ts" />
+/// <reference path="typings/halson/halson.d.ts" />
 
+import fs = require('fs');
 import express = require('express');
 import expressStaticZip = require('express-static-zip');
+import halson = require('halson');
+
 import GoldenTestData = require('./GoldenTestData');
 
 var gtd = new GoldenTestData();
@@ -11,8 +15,35 @@ var gtd = new GoldenTestData();
 var app = express();
 var port = Number(process.env.PORT || 8080);
 
+var STATIC_FOLDER = __dirname + '/static';
+var BASE_URI = 'http://localhost:' + port;
+
 // serving 3rd party code from ZIP file, just because statistics in Github pages ;-)
-app.use('/hal', expressStaticZip(__dirname + '/static/hal.zip'));
+app.use('/hal', expressStaticZip(STATIC_FOLDER + '/hal.zip'));
+
+app.get('/mobs', (req, res) => {
+  res.set('Content-Type', 'application/hal+json');
+  var resource = halson([ /* mob array */ ])
+    .addLink('self', {href : '/mobs', title : 'Mob Collection'});
+  res.send(resource);
+});
+
+app.get('/', (req, res) => {
+  var accepts = req.accepts(['html', 'json']);
+  if (accepts === 'html') {
+    res.redirect(302, '/hal/browser.html');
+  } else if (accepts === 'json') {
+    res.set('Content-Type', 'application/hal+json');
+    fs.readFile(STATIC_FOLDER + '/index.hal.json', {autoClose : true, flag : 'r', encoding : 'utf-8'}, function (err, data) {
+      if (err) throw err;
+      data = data.replace(/\$\{baseUri\}/g, BASE_URI);
+      res.send(data);
+    });
+  } else {
+    res.send(406, "The requested resource is only capable of generating content in JSON or HTML, but not acceptable according to the Accept headers sent in the request.")
+  }
+});
+
 
 app.listen(port);
 console.log('Express started on port ' + port);
---
+++ server/src/typings/halson/halson.d.ts
@@ -0,0 +1,9 @@
+declare module "halson" {
+
+  // minimalistic declaration file, which is needed by Typescript compiler
+
+  function halson(data?:any): any;
+
+  export = halson;
+
+}
