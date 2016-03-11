26bb22cd62e04940f9cda749e254fedbe9406744
nitram509 <maki@bitkings.de>
2016-03-11 02:01:04 +0100

    fix test

--- playground-server-nodejs/tests/GoldenTestData_spec.ts
+++ playground-server-nodejs/tests/GoldenTestData_spec.ts
@@ -2,7 +2,7 @@
 /// <reference path="typings/jasmine/jasmine.d.ts" />
 
 import GoldenTestData = require("../src/GoldenTestData");
-describe("Person FullName", () => {
+describe("Golden Test Data", () => {
 
   var goldenTestData:GoldenTestData;
 
@@ -13,6 +13,7 @@ describe("Person FullName", () => {
   it("golden test data generate at least one chicken", () => {
     var data = goldenTestData.create();
     expect(data.length).toBeGreaterThan(0);
+    expect(data[0].name).toEqual("Chicken");
   });
 
 });
